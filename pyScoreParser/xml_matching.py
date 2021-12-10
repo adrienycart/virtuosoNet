f2e67cd63ded4246c3515d44391d0eda7ca8dbf3
Adrien Ycart <adrienycart@nomadmusic.fr>
2021-12-10 15:19:28 +0100

    feat(pyScoreParser.xml_matching): include part names so they can be saved in multitrack midi

--- pyScoreParser/xml_matching.py
+++ pyScoreParser/xml_matching.py
@@ -1499,7 +1499,7 @@ def interpolation(a, list1, list2, index):
     return b1+ (a-a1) / (a2-a1) * (b2 - b1)
 
 
-def save_midi_notes_as_piano_midi(midi_notes, midi_pedals, output_name, bool_pedal=False, disklavier=False):
+def save_midi_notes_as_piano_midi(midi_notes, midi_pedals, output_name, part_names=None, bool_pedal=False, disklavier=False):
     piano_midi = pretty_midi.PrettyMIDI()
     piano_program = pretty_midi.instrument_name_to_program('Acoustic Grand Piano')
     if isinstance(midi_notes[0], list): #multi instruments
@@ -1517,6 +1517,9 @@ def save_midi_notes_as_piano_midi(midi_notes, midi_pedals, output_name, bool_ped
             midi_pedals[i].append(last_pedal)
             piano.control_changes = midi_pedals[i]
             piano_midi.instruments.append(piano)
+
+            if part_names is not None:
+                piano.name = part_names[i]
 
         # last_note_end = max([x[-1].end for x in midi_notes])
 
@@ -1821,7 +1824,9 @@ def read_xml_to_array(path_name, means, stds, start_tempo, composer_name, vel_st
         test_x.append(temp_x)
         note_locations.append(feat.note_location)
 
-    return test_x, xml_notes, xml_object, edges, note_locations
+    part_names = [part.score_part.part_name for part in xml_object.parts]
+
+    return test_x, xml_notes, xml_object, edges, note_locations, part_names
 
 
 def find_tempo_change(xml_notes):
